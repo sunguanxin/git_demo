525bf0bfb5425d844320f17a561f494eada80da0
xin <xin@qq.com>
2019-05-18 11:24:07 +0800

    第一次架构提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @version 1.0
+ * @user SGX
+ * @date 2019/5/18 11:20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello gitHub!");
+    }
+}
