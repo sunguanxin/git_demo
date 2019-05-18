5c976a8efd1bd89f29e1815f73951a32f59df4ff
xin <xin@qq.com>
2019-05-18 11:30:04 +0800

    修改了一些内容

--- src/main/java/com/qf/Main.java
+++ src/main/java/com/qf/Main.java
@@ -8,5 +8,6 @@ package com.qf;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello gitHub!");
+        System.out.println("hello gitHub!");
     }
 }
